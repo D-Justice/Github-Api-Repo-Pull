bc34401a4c6efd44ab6b6016552b7cd2bfb2f8d9
DJustice96 <damounderscore1019@hotmail.com>
2021-02-25 21:07:44 +1100

    Simplified code using correct json method

--- Repos_repo.py
+++ Repos_repo.py
@@ -1,29 +1,21 @@
 import requests
-import token_auth
 import re
 import json
 user = input("Input GitHub User-name: ")
 
 
 
-token = token_auth.token
 repo = requests.get("https://api.github.com/users/{}/repos".format(user))
 rate = requests.get("https://api.github.com/rate_limit")
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-result = json.dumps(repo.json())
-result = result.split()
-string = ""
-for item in result:
-    string += item
-find = re.findall('"name":"[^\"]+', string)
-#print(string)
-
-for i in find:
-    print(i.title().replace('"', ""))
-
+for item in repo.json():
+    try:
+        print(item['name'])
+    except Exception as i:
+        print(i)
 
 
 jprint(rate.json()['rate']['remaining'])
