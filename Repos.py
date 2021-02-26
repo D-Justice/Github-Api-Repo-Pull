0b92b69ae21292a1eac7a87849102461c0e43d89
DJustice96 <damounderscore1019@hotmail.com>
2021-02-27 05:45:29 +1100

    Now cycles through all pages of repositories

--- Repos.py
+++ Repos.py
@@ -4,6 +4,7 @@ import token_auth
 import json
 import time
 pagenumber = 1
+
 def userRepos():
 
     #prints out username, all public repository names and how many followers each one has
@@ -12,8 +13,7 @@ def userRepos():
     rcount = 0
     count = 0
     pagenumber += 1
-    if repo.status_code != 200:
-        print('broken')
+        
     print("\nDisplaying page {} of {}'s repositories".format(pagenumber, user))
     try:
         for item in repo.json():
@@ -25,11 +25,13 @@ def userRepos():
             try:
                 rcount += 1
                 print(('Repo #{}:').format(rcount),item['name'],'has------',item['watchers'],'watchers')
-            
+                if item['name'] == "":
+                    print("FUCKYES")
+                    break
             except Exception as i:
                 print(i)
     except:
-        print('Failed')
+        break#THIS IS THE KEY LINE (HOPEFULLY)
 def userFollowers():
     global user
     global newusercount
@@ -57,18 +59,17 @@ def jprint(obj):
     print(text)
 rate = requests.get("https://api.github.com/rate_limit")
 i = rate.json()['rate']['remaining']
-status = requests.get("https://api.github.com")
-print(type(status))
+repo = requests.get("https://api.github.com/users/{}/repos?page={}".format(user, pagenumber))
 newusercount = 0
 usernames = list()
 jprint(i)
 for i in range(howmany):
-    while status.ok:
-        if status.ok == False:
-            pass
+    while repo.status_code == 200:
+        print(repo.status_code)
+        if repo.status_code != 200:
+            break
         else:
             userRepos()
-            continue
     userFollowers()
         
 #prints all of the user followers names, adds the first unique one it comes across to 'usernames'
