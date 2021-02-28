493490bc4fdb4adae856ab477e2efe25ffac71a1
D-Justice <damounderscore1019@hotmail.com>
2021-02-28 21:36:11 +1100

    Now runs through all repository pages of a user before finding new unique user, rinse and repeat

--- Repos.py
+++ Repos.py
@@ -1,37 +1,34 @@
 import requests
 import re
-import token_auth
 import json
 import time
 pagenumber = 1
-
+totalcount = 0
 def userRepos():
 
     #prints out username, all public repository names and how many followers each one has
     global pagenumber
+    global totalcount
     repo = requests.get("https://api.github.com/users/{}/repos?page={}".format(user, pagenumber))
     rcount = 0
     count = 0
     pagenumber += 1
-        
     print("\nDisplaying page {} of {}'s repositories".format(pagenumber, user))
     try:
         for item in repo.json():
             newusercount = 0
             if count >= 1:pass
-            else: 
+            else:
                 count += 1
-                print('\nUsername:',item['owner']['login'],"\n\n") 
+                print('\nUsername:',item['owner']['login'],"\n\n")
             try:
                 rcount += 1
-                print(('Repo #{}:').format(rcount),item['name'],'has------',item['watchers'],'watchers')
-                if item['name'] == "":
-                    print("FUCKYES")
-                    break
+                totalcount += 1
+                print(('Repo #{}:').format(totalcount),item['name'],'has------',item['watchers'],'watchers')
             except Exception as i:
                 print(i)
     except:
-        break#THIS IS THE KEY LINE (HOPEFULLY)
+        print('Failed')
 def userFollowers():
     global user
     global newusercount
@@ -49,9 +46,10 @@ def userFollowers():
                 newusercount += 1
                 usernames.append(user)
                 user = users['login']
-                    
+
         else:
             continue
+
 user = input("Input GitHub User-name: ")
 howmany = int(input("Enter how many additional users info you'd like to see:"))
 def jprint(obj):
@@ -59,23 +57,36 @@ def jprint(obj):
     print(text)
 rate = requests.get("https://api.github.com/rate_limit")
 i = rate.json()['rate']['remaining']
+
 repo = requests.get("https://api.github.com/users/{}/repos?page={}".format(user, pagenumber))
-newusercount = 0
+
 usernames = list()
 jprint(i)
+howmany = (howmany * 2) + 1
+def totalRepoUser():
+    totalRepo = requests.get("https://api.github.com/users/{}".format(user))
+    global totalRepoNumber
+    totalRepoNumber = totalRepo.json()['public_repos']
+
+    print(totalRepoNumber)
 for i in range(howmany):
-    while repo.status_code == 200:
-        print(repo.status_code)
-        if repo.status_code != 200:
-            break
-        else:
+    totalRepoUser()
+    print(i)
+    newusercount = 0
+    pagenumber = 1
+    if totalcount == totalRepoNumber:
+        userFollowers()
+        totalcount = 0
+
+    else:
+        while totalcount < totalRepoNumber:
             userRepos()
-    userFollowers()
-        
+            print(totalcount, totalRepoNumber)
+
 #prints all of the user followers names, adds the first unique one it comes across to 'usernames'
 #and then navigates to their page to repeat the proccess'
-        
-    
+
+
 
 
 #jprint(followers.json())
